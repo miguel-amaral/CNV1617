fc5bae7e36fa9ed97377defb1adc5245b0439033
rafa32 <rafa_santos32@hotmail.com>
2017-04-14 18:28:33 +0100

    Tool class + logger: metrics to be done

---
+++ src/tool/Logger.java
@@ -0,0 +1,51 @@
+package tool;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Logger {
+
+	private static String FILENAME = "default.txt";
+
+  public Logger(){}
+
+  public Logger(String file_name){
+
+    this.FILENAME = file_name + ".txt";
+  }
+
+	public static void writeLine(String line) {
+
+		BufferedWriter bw = null;
+
+		try {
+
+			bw = new BufferedWriter(new FileWriter(FILENAME, true));
+			bw.write(line);
+			bw.newLine();
+
+			System.out.println("Done");
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (bw != null)
+					bw.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+
+	}
+
+}
