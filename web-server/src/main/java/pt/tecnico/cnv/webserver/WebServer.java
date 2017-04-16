29e06fe3712cf0892aa09283214278896089cf7f
Miguel Amaral <miguel.amaral04@gmail.com>
2017-04-16 18:51:26 +0100

    now link to image

--- web-server/src/main/java/pt/tecnico/cnv/webserver/WebServer.java
+++ web-server/src/main/java/pt/tecnico/cnv/webserver/WebServer.java
@@ -1,11 +1,11 @@
 package pt.tecnico.cnv.webserver;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
@@ -17,6 +17,7 @@ public class WebServer {
 		System.out.println("Booting server");
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/r.html", new MyHandler());
+        server.createContext("/images/", new GetImageHandler());
         server.setExecutor(Executors.newCachedThreadPool()); // creates a default executor
         server.start();
 		System.out.println("Server is now running");
@@ -36,8 +37,12 @@ public class WebServer {
 			try {
 				InvokeRay ray = new InvokeRay(query);
 				ray.execute();
-				message = "file: " + ray.outputFileName();
+                String ip = IpFinder.getMyIp() + ":8000/";
+                String localhost =  "localhost:8000/";
+				message = "<p>Amazon AWS context: <a href=\""+ip+ray.outputFileName()+"\">"+ip+ray.outputFileName()+"</a></br></br></br></br></br></br>";
+				message += "Home context: <a href=\""+localhost +ray.outputFileName()+"\">"+localhost +ray.outputFileName()+"</a></p>";
                 requestStatus = 200;
+                System.out.println(ip);
 			} catch (InvalidArgumentsException e) {
 				e.printStackTrace();
 				message = "Error: InvalidArgumentsException";
@@ -53,7 +58,41 @@ public class WebServer {
             os.close();
         }
     }
+    static class GetImageHandler implements HttpHandler{
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            String filePath = he.getRequestURI().toString().substring(1);;
+            System.out.println("The filePath: " + filePath );
+
+            Headers headers = he.getResponseHeaders();
+            headers.add("Content-Type", "image/bmp");
+
+            File file = new File(filePath);
+            byte[] bytes  = new byte [(int)file.length()];
+            System.out.println(file.getAbsolutePath());
+            System.out.println("length:" + file.length());
+
+            OutputStream outputStream = he.getResponseBody();
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+                bufferedInputStream.read(bytes, 0, bytes.length);
+                if(bytes.length == 0) {
+                    throw new IOException("File not found : " + filePath);
+                }
+                he.sendResponseHeaders(200, file.length());
+                outputStream.write(bytes, 0, bytes.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+                String message = "Error: " + e.getMessage();
+                he.sendResponseHeaders(400, message.length());
+                outputStream.write(message.getBytes());
+            }
+            outputStream.close();
 
 
+        }
+    }
 
 }
