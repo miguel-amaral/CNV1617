5e462c1ba091940c55ba23c61d4e662fa22c8750
Andre Dias <jdias.email@gmail.com>
2017-04-17 13:28:17 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 cd $(mktemp -d) #go to new temporary dir
-git clone git@github.com:miguel-amaral/CNV1617.git
-cd CNV1617/
 mvn clean install
 cd Instrumentation/src/
 export CLASSPATH=$CLASSPATH:$(pwd)
