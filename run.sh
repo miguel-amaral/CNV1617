71c34a1d4010087886cbb6c1db27c5d97b262397
rafa32 <rafa_santos32@hotmail.com>
2017-04-17 11:33:24 +0100

    minor change on init script

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@ cd $(mktemp -d) #go to new temporary dir
 git clone git@github.com:miguel-amaral/CNV1617.git
 cd CNV1617/
 mvn clean install
-cd web-server/
+cd Instrumentation/src/
+export CLASSPATH=$CLASSPATH:$(pwd)
+cd ../../web-server/
 mvn exec:java
