08d140e8130a23815897ed681d613c317ac1ace7
Miguel Amaral <miguel.amaral04@gmail.com>
2017-05-11 16:50:13 +0100

    common, first shitty load balancer

--- loadbalancer/src/main/java/pt/tecnico/cnv/loadbalancer/ProcessQuery.java
+++ loadbalancer/src/main/java/pt/tecnico/cnv/loadbalancer/ProcessQuery.java
@@ -2,24 +2,29 @@ package pt.tecnico.cnv.loadbalancer;
 
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.model.Instance;
+import pt.tecnico.cnv.common.HttpAnswer;
+import pt.tecnico.cnv.common.HttpRequest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by miguel on 10/05/17.
  */
 public class ProcessQuery {
 
-    protected String _query;
-    private int _status = 400;
-    private String _message = "Error :(";
     private AmazonEC2 ec2;
 
-    List<Instance>
+    List<Instance> _known_instances = new ArrayList<Instance>();
+    List<String> _removed_instances = new ArrayList<String>();
 
-    public ProcessQuery(AmazonEC2 ec2, String query)  {
-        _query = query;
+    Map<String, Container> _instances = new HashMap<String, Container>();
+
+    public ProcessQuery(AmazonEC2 ec2)  {
         this.ec2 = ec2;
+        updateInstances();
     }
 
     public int metricValue() {
@@ -27,25 +32,80 @@ public class ProcessQuery {
         return 1;
     }
 
-    public String process() {
+    public HttpAnswer process(String query) {
 
-
-
+        //update all instances ??
+        updateInstances();
+        Instance lowestInsance = getLighestMachine();
+        String ip = lowestInsance.getPublicIpAddress();
+        System.out.println("lowest ip: " + ip);
+        HttpAnswer answer = HttpRequest.sendGet(ip+":8000/r.html?"+query,new HashMap<String, String>());
+        return answer;
     }
 
 
     private void updateInstances() {
         List<Instance> instances = new ListWorkerInstances(ec2).listInstances();
         for(Instance instance : instances) {
-
+            String id = instance.getInstanceId();
+            if(!_instances.containsKey(id)) {
+                this.newInstance(instance);
+            }
         }
     }
 
-    public String response() {
-        return _message;
+    private void newInstance(Instance instance) {
+        String id = instance.getInstanceId();
+        if(_removed_instances.contains(id)) {
+            //it is shutting down ..
+            return;
+        }
+        if(this.instanceIsReady(instance)){
+            _instances.put(id,new Container(instance,0));
+        }
     }
 
-    public int status() {
-        return _status;
+    private boolean instanceIsReady(Instance instance) {
+        String publicIpAddress = instance.getPublicIpAddress();
+
+        return true; //TODO
+
     }
+
+    public String toString() {
+        String newLine = "\n";
+        StringBuilder toReturn = new StringBuilder("Instances:" + newLine);
+        for(Map.Entry<String,Container> entry : _instances.entrySet()){
+            toReturn.append(entry.getKey()).append(" : ").append(entry.getValue().metric);
+        }
+        toReturn.append(newLine).append("Removed:").append(newLine);
+        for (String id : _removed_instances) {
+            toReturn.append(id).append(newLine);
+        }
+        return toReturn.toString();
+    }
+
+    public Instance getLighestMachine() {
+        int min = 0; //max int
+        Instance minInstance = null;
+        for(Map.Entry<String,Container> entry : _instances.entrySet()){
+            if(entry.getValue().metric < min || minInstance == null) {
+                minInstance = entry.getValue().instance;
+                min = entry.getValue().metric;
+            }
+        }
+        return minInstance;
+    }
+
+    class Container {
+        Instance instance;
+        int metric;
+        Container(Instance instance, int metric) {
+            this.instance = instance;
+            this.metric = metric;
+        }
+    }
+
+
+
 }
--- run_loadbalancer.sh
+++ run_loadbalancer.sh
@@ -1,1 +1,2 @@
 echo "Its a brave new world!"
+mvn clean install -q && mvn -pl loadbalancer exec:java -q
