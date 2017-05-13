2b52884d0981e80d482a898bd06b69f917636b12
Miguel Amaral <miguel.amaral04@gmail.com>
2017-05-13 22:31:00 +0100

    ws now contacts mss

--- web-server/src/main/java/pt/tecnico/cnv/webserver/WebServer.java
+++ web-server/src/main/java/pt/tecnico/cnv/webserver/WebServer.java
@@ -133,6 +133,7 @@ public class WebServer {
 
                 HttpAnswer answer_storage_server = HttpRequest.sendGet(finalEndpoint.toString());
                 System.out.println(answer_storage_server);
+                message += "<b>Storage server answer: </b>" +  "<pre>" + answer_storage_server+ "\n </pre>";
 
             } catch (InvalidArgumentsException e) {
 				e.printStackTrace();
