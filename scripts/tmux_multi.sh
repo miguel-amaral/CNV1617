6d8fcaaa4b91f75f4227b2d7dcc184499c37042b
Miguel Amaral <miguel.amaral04@gmail.com>
2017-05-19 13:25:55 +0100

    update

--- scripts/tmux_multi.sh
+++ scripts/tmux_multi.sh
@@ -1,42 +1,43 @@
 #!/usr/bin/env bash
+session_name=$1
+session_name="oals_2"
 users=16
 number=200
-session_name="my_session_4"
 tmux kill-session -t ${session_name}
 endpoint="load-balancer-cnv.tk:8000/r.html?f=test04.txt&sc=${number}&sr=${number}&wc=${number}&wr=${number}&coff=0&roff=0"
 script="while true ; do wget \"$endpoint\" -o /dev/null -O /dev/null ;echo \"tick\"; done"
-echo ${script}
-echo ${session_name}
-tmux new-session -d -s ${session_name} ${script}
+echo "${script}"
+echo "${session_name}"
+tmux new-session -d -s ${session_name} "${script}"
 tmux select-window -t ${session_name}:0
-tmux split-window -h ${script}
-tmux split-window -v -t 0 ${script}
-tmux split-window -v -t 1 ${script}
+tmux split-window -h "${script}"
+tmux split-window -v -t 0 "${script}"
+tmux split-window -v -t 1 "${script}"
 #4 achieved
 if [ ${users} -gt 4 ]
 then
-tmux split-window -h -t 0 ${script}
-tmux split-window -h -t 1 ${script}
-tmux split-window -h -t 2 ${script}
-tmux split-window -h -t 3 ${script}
+tmux split-window -h -t 0 "${script}"
+tmux split-window -h -t 1 "${script}"
+tmux split-window -h -t 2 "${script}"
+tmux split-window -h -t 3 "${script}"
 fi
 #8 achieved
 
 if [ ${users} -gt 8 ]
 then
-tmux split-window -v -t 0 ${script}
-tmux split-window -v -t 1 ${script}
-tmux split-window -v -t 2 ${script}
-tmux split-window -v -t 3 ${script}
+tmux split-window -v -t 0 "${script}"
+tmux split-window -v -t 1 "${script}"
+tmux split-window -v -t 2 "${script}"
+tmux split-window -v -t 3 "${script}"
 fi
 #12
 
 if [ ${users} -gt 12 ]
 then
-tmux split-window -v -t 4 ${script}
-tmux split-window -v -t 5 ${script}
-tmux split-window -v -t 6 ${script}
-tmux split-window -v -t 7 ${script}
+tmux split-window -v -t 4 "${script}"
+tmux split-window -v -t 5 "${script}"
+tmux split-window -v -t 6 "${script}"
+tmux split-window -v -t 7 "${script}"
 fi
 if [ ${users} -gt 16 ]
 then
